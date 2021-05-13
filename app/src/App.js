a0d412b302773275d98930b485ba05c273fa1fce
jonaspetraska7 <jonas.petraska7@gmail.com>
2021-05-13 12:59:48 +0300

    Initial commit
    * Created react app

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Content">
+          First build
+      </div>
+    </div>
+  );
+}
+
+export default App;
